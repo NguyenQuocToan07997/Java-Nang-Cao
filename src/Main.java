3163ec92ba39cfb20a787cff7ae152748b5160d9
NguyenQuocToan07997 <toannguyen31k11@viendong.edu.vn>
2020-01-02 16:14:33 +0700

    thi giua ky

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main{
 
     public static void main(String[] args) {
         StudentsModel model = new StudentModel_lmpl();
-        StudentListView studentListView = new StudentListView(model);
+        StudentListView studentsListView = new StudentListView(model);
 
     }
 }
